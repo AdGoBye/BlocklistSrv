1fadd7da10d125c566f90c02885ce0012d67f721
Regalia <regalia@tutanota.de>
2024-06-07 17:47:18 +0200

    test(blocklists): Add test for generateObjectIndex
    Most people would decide the effort of making a literal object for this is too much and absurd, thankfully for this project I'm not most people.

--- blocklists_test.go
+++ blocklists_test.go
@@ -190,3 +190,25 @@ func Test_stringToHash(t *testing.T) {
 		})
 	}
 }
+
+func Test_generateObjectIndex(t *testing.T) {
+	type args struct {
+		blocklistsLocations []string
+	}
+	tests := []struct {
+		name        string
+		args        args
+		wantMapping map[string]WorldObject
+	}{
+		{
+			"index object from agbcommunity 5646b6d",
+			args{blocklistsLocations: []string{"https://raw.githubusercontent.com/AdGoBye/AdGoBye-Blocklists/5646b6d5aecf00d336184bd70fd4c090b3a25f86/AGBCommunity.toml"}},
+			map[string]WorldObject{"//72sZH9E1KefGiDV2vl6hS7tPFxRczk4f8tozmZT+A=": {FriendlyName: "The Black Cat", GameObjectMapping: map[string]Gameobject{"1rjfo3+mVV3ZAVcJI6voGUkdaE+MLvFyI0PMBzyG1XY=": {Name: "cork medium", Position: Pointer(GameobjectPosition{X: 26.773, Y: 3.244, Z: -13.982}), Parent: nil}}}, "UisKnWNb5njDLfcjjdHEql4PcSNaWkRudA2yXXDuAZQ=": {FriendlyName: "Furry Hideout", GameObjectMapping: map[string]Gameobject{"44+cpG4N7ycmV+zAvHftxlqjjIKtgIWwPBrRR6ECQfY=": {Name: "Cube (5)", Position: Pointer(GameobjectPosition{X: -29.597, Y: 44.894, Z: 6.501}), Parent: nil}, "eZpZL6VdV6MIwt5Zp85xa/bCb1uQvr7aNwMAa9Ci6r4=": {Name: "PPSUI (2)", Position: nil, Parent: nil}}}, "Wn6QIbBCeSTvZLenudccQBxdux4AbFIsCSRuiqFk0wc=": {FriendlyName: "Furry Talk and Chill", GameObjectMapping: map[string]Gameobject{"oZZfW96QQe/r97oBD+MJcFomNy/hpNTR3OeVj1QouWo=": {Name: "Bottom Tex", Position: nil, Parent: nil}}}, "ejki17vqRmaOoosLxZ7Btg9c0RP6Q4hl2vClhV6Mxjc=": {FriendlyName: "Murder 4", GameObjectMapping: map[string]Gameobject{"E+vtKYtVJUe5ZB6Exc9r6wtNhCTV7HoI1hPDVGByNr8=": {Name: "Link (2)", Position: nil, Parent: nil}}}, "ekogGjm7rMq9nDvpj5zvEnl6hu9kZFSbUTG61Uj+YzM=": {FriendlyName: "Prison Escape!", GameObjectMapping: map[string]Gameobject{"/JIRSiPxW8PbT3/6hiteuy19SvYXKRkiOF000vaz4IA=": {Name: "Image (3)", Position: Pointer(GameobjectPosition{X: -175, Y: -175, Z: 0}), Parent: Pointer(Gameobject{Name: "Panel"})}, "0H9wPe2SQozFHxSczgUCQX4HGA++4FiQ8NcXdh43mF8=": {Name: "Group Sign", Position: nil, Parent: nil}, "79fSWuUcG0xQRv3NVFxCZ08yq4Ir7+28e0BxhdBF+Lk=": {Name: "Text (TMP) (2)", Position: Pointer(GameobjectPosition{X: 18, Y: -175, Z: 0}), Parent: Pointer(Gameobject{Name: "Panel"})}, "NW0R88MKtatWNgpqkGRxsAPx+YYasT5FFh51sJPoO7w=": {Name: "Text (TMP) (1)", Position: Pointer(GameobjectPosition{X: 14, Y: -135, Z: 0}), Parent: Pointer(Gameobject{Name: "Panel"})}, "gVTW8UCy//rVmmcMU7GHTCvIl2rikJie1XDMoRZgIRU=": {Name: "Text (TMP)", Position: Pointer(GameobjectPosition{X: -2.5, Y: -95, Z: 0}), Parent: Pointer(Gameobject{Name: "Panel"})}, "hl4nmC0Va0NkjmiEd2yhKYBdwB0V95vxy1xNiQ+iRdo=": {Name: "Image (2)", Position: Pointer(GameobjectPosition{X: -140, Y: -135, Z: 0}), Parent: Pointer(Gameobject{Name: "Panel"})}, "mHCtrK1xyvq2zsj6JCg6GONmgzYE2hdQpp/vamFQURQ=": {Name: "Image (1)", Position: Pointer(GameobjectPosition{X: -78.4, Y: -95, Z: 0}), Parent: Pointer(Gameobject{Name: "Panel"})}}}, "jtLZiuvHwfHGwgUPKXv+znfckOxuWp7/PHoLQKlQcPs=": {FriendlyName: "Just B Club 3", GameObjectMapping: map[string]Gameobject{"2nK7NHwf3fLLVlNJkj3PsOm2St2M4U6Am0jNj1L9IXk=": {Name: "TV Prefab UNIQUE", Position: Pointer(GameobjectPosition{X: -4.366071, Y: 3.072498, Z: -54.21133}), Parent: nil}, "VaRJlJS9rr63cwr8+UirDj+/e7OqqAnwMykRccS9Xxs=": {Name: "Discord TV Ad (1)", Position: nil, Parent: nil}, "vm9Zs730Y6b3lkWASmTsoCxawEFZCUBOLADwjGtmFp0=": {Name: "Poster (9)", Position: nil, Parent: Pointer(Gameobject{Name: "Poster (9)"})}}}, "sYREJ3yvphTlFAF97DgXjRtoiLfWWoN1FcKOAelhPYs=": {FriendlyName: "The room of the rain", GameObjectMapping: map[string]Gameobject{"H3EGdEcNRGq2YGF2NnYj+XdjmJIFm7x50b1bgR3Er1Q=": {Name: "Patreon ui", Position: nil, Parent: Pointer(Gameobject{Name: "Patreon Things"})}, "H8sLfASKE2wV/UU/5/XYOOqxpSjKqtNbE2GLnDH1zys=": {Name: "Neverphone", Position: nil, Parent: nil}, "PSl0xoSem8oYbhJR2yH/GGCw3Q6xyN+WkX1GIiUjErA=": {Name: "Patreon texture Changer (1)", Position: nil, Parent: Pointer(Gameobject{Name: "Patreon Things"})}, "S8gfXVTsPW+pRlqKbf2bBKOS2AQ156bjRy8QHMxET7Y=": {Name: "Patreon panel", Position: nil, Parent: Pointer(Gameobject{Name: "Patreon Things"})}, "rRUqUuTSY4tekMftvMmKN/6ecwbED4yy4hGH/dITo3w=": {Name: "Patreon texture Changer", Position: nil, Parent: Pointer(Gameobject{Name: "Patreon Things"})}}}, "sbAfDcLEMfXdqt7ymd5y5wtGDnQHIa5oFqfjSnxSv+8=": {FriendlyName: "Default Home", GameObjectMapping: map[string]Gameobject{"tAyJF6Ht0JkOMLMlZI/5hacz365Y+DJSaNgayRDazkg=": {Name: "posterlight (8)", Position: nil, Parent: nil}}}, "zPjucIpmtcG2w2sUUVaq7j0tuRKLQINxrLRDEiJv+ZQ=": {FriendlyName: "Movie & Chill", GameObjectMapping: map[string]Gameobject{"NZk/vsuk5MZgw00AgfSzItvXCUMaCRgJTMVEA/pyXAc=": {Name: "Label (2)", Position: nil, Parent: nil}}}},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.wantMapping, generateObjectIndex(tt.args.blocklistsLocations), "generateObjectIndex(+%v)", tt.args.blocklistsLocations)
+		})
+	}
+}
